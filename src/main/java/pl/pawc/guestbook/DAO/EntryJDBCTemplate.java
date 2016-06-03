2445bde68470135842091824fc9faedcaea58d3f
pawc <pswid89@gmail.com>
2016-06-03 22:20:22 +0200

    Revert "setting name for session parameter"
    This reverts commit 7ed943ad26f2cbb8d003e471375b7926861655ff.

--- src/main/java/pl/pawc/guestbook/DAO/EntryJDBCTemplate.java
+++ src/main/java/pl/pawc/guestbook/DAO/EntryJDBCTemplate.java
@@ -7,32 +7,34 @@ import java.util.List;
 
 public class EntryJDBCTemplate implements EntryDAO{
 
-private DataSource dataSource;
-private JdbcTemplate jdbcTemplateObject;
+	private DataSource dataSource;
+	private JdbcTemplate jdbcTemplateObject;
 
-  public void setDataSource(DataSource dataSource){
-    this.dataSource = dataSource;
-    this.jdbcTemplateObject = new JdbcTemplate(dataSource);
-  }
+	public void setDataSource(DataSource dataSource){
+		this.dataSource = dataSource;
+		this.jdbcTemplateObject = new JdbcTemplate(dataSource);
+	}
 
-  @Override
-  public Entry getEntry(int id){
-    String SQL = "Select * from Guestbook where id = ?";
-    Entry entry = jdbcTemplateObject.queryForObject(SQL, new Object[]{id}, new EntryMapper());
-    return entry;
-  }
+	@Override
+	public Entry getEntry(int id){
+		String SQL = "Select * from Guestbook where id = ?";
+		Entry entry = jdbcTemplateObject.queryForObject(SQL, new Object[]{id}, new EntryMapper());
+		return entry;
+	}
 
-  @Override
-  public List<Entry> getAllEntries(){
-    String SQL = "select * from Guestbook order by id DESC";
-    List<Entry> entries = jdbcTemplateObject.query(SQL, new EntryMapper());
-    return entries;
-  }
+	@Override
+	public List<Entry> getAllEntries(){
+		String SQL = "select * from Guestbook order by id DESC";
+		List<Entry> entries = jdbcTemplateObject.query(SQL, new EntryMapper());
+		return entries;
+	}
 
-  @Override
-  public void addEntry(String name, String message){
-    String SQL = "insert into Guestbook (name, message) values (?, ?)";
-    jdbcTemplateObject.update(SQL, new Object[]{name, message} );
-  }
+	@Override
+	public void addEntry(String name, String message){
+		String SQL = "insert into Guestbook (name, message) values (?, ?)";
+		jdbcTemplateObject.update(SQL, new Object[]{name, message} );
+	}
 
 }
+
+

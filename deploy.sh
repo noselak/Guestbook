3fad9ccbd81928be22ccfb6f736cde5975c0dc90
pawc <pswid89@gmail.com>
2016-06-07 09:46:47 +0200

    deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,1 +1,2 @@
-mvn clean package; sudo cp target/Guestbook-1.war /opt/apache-tomcat-8.0.33/webapps/Guestbook.war
+mvn clean package
+sudo cp target/Guestbook-1.war /usr/share/tomcat7/webapps/Guestbook.war
